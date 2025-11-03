b1677f7a82d2d7e0a444f2a90ff39c6c460d5812
Edzai Nhema <nhemaed@gmail.com>
2025-11-03 06:03:23 -0400

    updated user system

--- here/views.py
+++ here/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 @api_view(['GET'])
+@permission_classes([AllowAny]) 
 def hello_world(request):
-	return Response({"message": "Hello World. How are you doing"})
-    
+	if request.user.is_authenticated:
+		message = f"Hello {request.user.username}, how are you doing?"
+	else:
+		message = "Hello stranger, how are you doing?"
+	
+	return Response({"message": message})
 # Create your views here.
