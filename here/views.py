e882c19388882889d25c4818cea6d9ebe8d44725
Edzai Nhema <nhemaed@gmail.com>
2025-11-03 05:30:15 -0400

    Small change to test new user system

--- here/views.py
+++ here/views.py
@@ -4,6 +4,6 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def hello_world(request):
-	return Response({"message": "Hello World"})
+	return Response({"message": "Hello World. How are you doing"})
     
 # Create your views here.
